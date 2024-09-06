aac849dc017e281ef2396ebbb98736f88e0afcbf
Justin Littman <justinlittman@stanford.edu>
2024-09-06 14:16:48 -0400

    Default to 3 pages.

--- app/services/pdf_support.rb
+++ app/services/pdf_support.rb
@@ -2,7 +2,7 @@
 
 # Methods for working with PDFs.
 class PdfSupport
-  def self.first_pages(path:, count: 2)
+  def self.first_pages(path:, count: 3)
     doc = HexaPDF::Document.open(path)
     (count..(doc.pages.count - 1)).to_a.reverse_each do |index|
       doc.pages.delete_at(index)
